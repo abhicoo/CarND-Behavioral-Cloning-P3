0786b9ba4bb11ff0d48eab247c553b891ed0334d
Dev <dev@nurtureco.co>
2017-10-26 22:59:56 +0530

    Added Left and Right Camera images in training

--- batch_generator.py
+++ batch_generator.py
@@ -13,11 +13,20 @@ def generator(samples, batch_size):
 			images = []
 			angles = []
 			for batch_sample in batch_samples:
-				name = './data/IMG/'+batch_sample[0].split('/')[-1]
-				center_image = cv2.imread(name)
+				#Loading Images
+				center_name = './data/IMG/'+batch_sample[0].split('/')[-1]
+				center_image = cv2.imread(center_name)
+				left_name = './data/IMG/'+batch_sample[1].split('/')[-1]
+				left_image = cv2.imread(left_name)
+				right_name = './data/IMG/'+batch_sample[2].split('/')[-1]
+				right_image = cv2.imread(right_name)
+				#Loading Steering
+				correction = 0.2
 				center_angle = float(batch_sample[3])
-				images.append(center_image)
-				angles.append(center_angle)
+				left_angle = center_angle + correction
+				right_angle = center_angle - correction
+				images.extend([center_image, left_image, right_image])
+				angles.extend([center_angle, left_angle, right_angle])
 			X_train = np.array(images)
 			y_train = np.array(angles)
 			yield shuffle(X_train, y_train)
